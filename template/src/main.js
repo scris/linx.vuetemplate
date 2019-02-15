6a2c6c5a7f0c5cb1cba95a26a254cd50416bfa11
Dominik <slaaker@126.com>
2019-02-15 15:45:41 +0800

    electron modification

--- template/src/main.js
+++ template/src/main.js
@@ -1,9 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+{{#isEnabled plugins 'axios'}}
+import axios from 'axios'
+{{/isEnabled}}
 import App from './App';
 import router from './router';
 import cordova from './mobile-index';
+{{#isEnabled plugins 'vuex'}}
+import store from './store'
+{{/isEnabled}}
+
+
 
 Vue.config.productionTip = false;
 
